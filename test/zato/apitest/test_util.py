c85ef10f53e10f0b674bbcc903d668e645188e77
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-06-01 22:07:31 +0200

    Tests for util.new_context.

--- test/zato/apitest/test_util.py
+++ test/zato/apitest/test_util.py
@@ -11,7 +11,37 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # stdlib
 from unittest import TestCase
 
+# Bunch
+from bunch import Bunch
+
+# Zato
+from zato.apitest import version
+from zato.apitest.util import context, new_context, rand_string
 
 class UtilTest(TestCase):
-    def test_new_context(self):
-        pass
+
+    def _test_new_context(self, ctx, environment_dir):
+        self.assertEquals(ctx.environment_dir, environment_dir)
+        self.assertDictEqual(ctx.user_data, {})
+        self.assertDictEqual(ctx.date_formats, {'default':'YYYY-MM-DDTHH:mm:ss'})
+        self.assertDictEqual(ctx.request.headers, {'User-Agent':'zato.apitest/{} (+https://zato.io)'.format(version)})
+        self.assertDictEqual(ctx.request.ns_map, {})
+        self.assertDictEqual(ctx, context)
+
+    def test_new_context_from_old_ctx(self):
+
+        # Done twice to ensure that util's context actually is wiped out.
+        for x in range(2):
+            environment_dir = rand_string()
+            old_ctx = Bunch()
+            old_ctx.zato = Bunch(environment_dir=environment_dir)
+            ctx = new_context(old_ctx, None)
+            self._test_new_context(ctx, environment_dir)
+
+    def test_new_context_from_environment_dir(self):
+
+        # Same comment as in test_new_context_from_old_ctx.
+        for x in range(2):
+            environment_dir = rand_string()
+            ctx = new_context(None, environment_dir)
+            self._test_new_context(ctx, environment_dir)
