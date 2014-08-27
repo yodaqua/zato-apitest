d07b798a0c2de8546dda4dba328ea9dbba586942
Filip Kłosowski <filip@zato.io>
2014-08-27 15:08:38 +0200

    GH #5 - Added steps related to uploading Zato services + unit tests.

---
+++ src/zato/apitest/steps/zato_.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2014 Filip Kłosowski <filip at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# Originally part of Zato - open-source ESB, SOA, REST, APIs and cloud integrations in Python
+# https://zato.io
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+import json
+from os.path import split
+from httplib import OK
+
+# requests
+import requests
+
+# Behave
+from behave import given, when
+
+# Zato
+from .. import util
+
+# ###############################################################################################################################
+
+@given('I store "{cluster_id}" "{url_path}" "{username}" "{password}" under Zato "{conn_name}"')
+@util.obtain_values
+def given_i_store_zato_info_under_conn_name(ctx, cluster_id, url_path, username, password, conn_name):
+    ctx.zato.user_ctx[conn_name] = {
+        'cluster_id': cluster_id,
+        'url_path': url_path,
+        'username': username,
+        'password': password
+    }
+
+@when('I upload a Zato service from "{module_path}" to "{conn_name}"')
+@util.obtain_values
+def when_i_upload_a_zato_service_from_path_to_conn_details(ctx, module_path, conn_name):
+    with open(module_path, 'r') as module:
+        service_code = module.read().encode('base64', 'strict')
+        payload = json.dumps(
+            {
+            'cluster_id': conn_name['cluster_id'],
+            'payload': service_code,
+            'payload_name': split(module_path)[-1]
+            },ensure_ascii=False)
+
+        upload = requests.get(
+            conn_name['url_path'], auth=(conn_name['username'], conn_name['password']), data=payload
+            )
+        assert upload.status_code == OK
---
+++ test/zato/apitest/steps/test_zato_.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2014 Filip Kłosowski <filip at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# Originally part of Zato - open-source ESB, SOA, REST, APIs and cloud integrations in Python
+# https://zato.io
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+from httplib import BAD_REQUEST
+from unittest import TestCase
+from StringIO import StringIO
+
+# Bunch
+from bunch import Bunch
+
+# mock
+from mock import patch, MagicMock, Mock
+
+# Zato
+from zato.apitest import util
+from zato.apitest.steps import zato_
+
+# ###############################################################################################################################
+
+class GivenTestCase(TestCase):
+    def setUp(self):
+        self.ctx = Bunch()
+        self.ctx.zato = util.new_context(None, util.rand_string(), {})
+
+        self.fake_service_code = util.rand_string()
+
+        conn_name = util.rand_string()
+        cluster_id = util.rand_int()
+        url_path = util.rand_string()
+        username = util.rand_string()
+        password = util.rand_string()
+        zato_.given_i_store_zato_info_under_conn_name(self.ctx, cluster_id, url_path, username, password, conn_name)
+        self.stored = self.ctx.zato.user_ctx[conn_name]
+
+    def test_given_i_store_zato_info_under_conn_name(self):
+        conn_name = util.rand_string()
+        cluster_id = util.rand_int()
+        url_path = util.rand_string()
+        username = util.rand_string()
+        password = util.rand_string()
+        zato_.given_i_store_zato_info_under_conn_name(self.ctx, cluster_id, url_path, username, password, conn_name)
+        stored = self.ctx.zato.user_ctx[conn_name]
+        # self.assertEquals(stored['cluster_id'], cluster_id)
+        # self.assertEquals(stored['url_path'], url_path)
+        # self.assertEquals(stored['username'], username)
+        # self.assertEquals(stored['password'], password)
+        for key in stored:
+            self.assertEquals(stored[key], eval(key))
+
+    def test_when_i_upload_a_zato_service_from_path_to_conn_details_fails(self):
+        file_mock = MagicMock(spec=file)
+        file_mock.__enter__.return_value = StringIO(self.fake_service_code)
+        with patch('__builtin__.open', create=True) as mock_open:
+            mock_open.return_value = file_mock
+            with patch('requests.get') as mock_requests:
+                mock_requests.return_value = mock_response = Mock()
+                mock_response.status_code = BAD_REQUEST
+                self.assertRaises(AssertionError, zato_.when_i_upload_a_zato_service_from_path_to_conn_details,self.ctx, './abc', self.stored)
